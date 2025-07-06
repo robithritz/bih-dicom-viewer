d16a218aa55976bda6cbda4d06ee119cb039d33f
robith <robithritz@gmail.com>
2025-07-07 06:38:54 +0700

    fix logo url

--- src/pages/portal.js
+++ src/pages/portal.js
@@ -193,7 +193,7 @@ export default function AdminPortal() {
           <div className="max-w-md w-full space-y-8">
             <div>
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 items-center flex flex-col">
-                <Image src="/images/bih-logo.png" alt="Logo" width={200} height={80} />
+                <Image src={`${router.basePath}/images/bih-logo.png`} alt="Logo" width={200} height={80} />
                 Admin Portal Login
               </h2>
               <p className="mt-2 text-center text-sm text-gray-600">
